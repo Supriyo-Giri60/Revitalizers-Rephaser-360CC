761aa4f16d93cf033060434101693b0dc6a8665d
Supriyo-Giri60 <supriyogiri803@gmail.com>
2025-03-09 10:53:01 +0530

    Update connectDB.js

--- BACKEND/DB/connectDB.js
+++ BACKEND/DB/connectDB.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+MONGO_URI = 'mongodb+srv://supriyogiri372:eLs0Q9ir5Y7Qhp9Z@cluster0.aid7c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
